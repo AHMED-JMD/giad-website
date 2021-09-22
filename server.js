e6b2211aa3aa45776eaddd75fd5e115cda3cef14
AHMED-JMD <ahmedabdalrahman706@gmail.com>
2021-09-22 03:03:41 -0700

    deploy final website

--- server.js
+++ server.js
@@ -29,57 +29,11 @@ app.get('/contact', (req,res) =>{
     res.sendFile(path.join(__dirname,'./public/contact.html'))
 });
 
-//english pages goes here
-app.get('/en', (req,res) =>{
-    res.sendFile(path.join(__dirname, './public/index-en.html'))
-});
-app.get('/about-en', (req,res) =>{
-    res.sendFile(path.join(__dirname, './public/about-en.html'))
-});
-app.get('/products-en', (req,res) =>{
-    res.sendFile(path.join(__dirname,'./public/products-en.html'));
-    res.sendFile(path.join(__dirname, './public'));
-});
-app.get('/services-centers-en', (req,res) =>{
-    res.sendFile(path.join(__dirname, './public/services-centers-en.html'))
-});
-app.get('/contact-en', (req,res) =>{
-    res.sendFile(path.join(__dirname,'./public/contact-en.html'))
-});
+
 
 
 //post rout
-app.post('/email-en', async (req, res,next) =>{
-    let {name,email, subject, text, phoneNum} = req.body;
-    
-    if(!name || !email || !subject || !text || !phoneNum){
-        return res.status(400).send({msg:'please enter all feilds'});
-    }
-//filter input feilds 
-name = xssFilter.inHTMLData(name),
-email = xssFilter.inHTMLData(email),
-subject = xssFilter.inHTMLData(subject),
-text = xssFilter.inHTMLData(text),
-phoneNum = xssFilter.inHTMLData(phoneNum)
-    
-   
-    //verify email address
-quickemailverification.verify(email, (err, response) =>{
-    console.log(response.body)
-})
 
-    sendMail(name, email, subject, text, phoneNum, (err, data) =>{            
-        if(err){
-            res.status(500).json({err: 'internal error', error:err.data})
-        }else{
-            res.json('Message Sent , We Will be in touch soon');
-        }
-               
-});
-   
-
-
-});
 //post 
 app.post('/email', async (req, res,next) =>{
     let {name,email, subject, text, phoneNum} = req.body;
@@ -112,13 +66,14 @@ phoneNum = xssFilter.inHTMLData(phoneNum)
                
 });
    
-
-
 });
+
+
+app.use('/en', require('./routes/en-route'));
 
 app.use( (req, res, next) =>{
     res.status(404).sendFile(path.join(__dirname, './public/404.html'));
-})
+});
 //listening to port
 const port = process.env.PORT || 7894;
 app.listen(port, ()=> console.log(`server running on port ${port}`));
